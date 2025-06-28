29e8e6a849691c298b47eaf0b65558f73e231d98
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-06-29 03:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 //your JS code here. If required.
 function leapyear(year) {
-	if((year%100!==0 || year%4===0 )|| year%400===0)
-		return true;
-	else
-		false;
+	return ((year%100!==0 && year%4===0 )|| year%400===0)
 }
 function daysOfAYea(year) {
-	let isleap=leapyear(year);
-	if(isleap)
-	{
-		return 366;
-	}
-	else{
-		return 365;
-	}
+	let isleap=leapyear(year)
+	return isleap?365:366;
 }
